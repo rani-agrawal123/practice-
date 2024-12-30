bab7cd64ffa92fca693bed28d37352eed1dddb37
rani-agrawal123 <agrawalrani2003@gmail.com>
2024-12-30 16:42:00 +0530

    sumArray

--- sumarr.js
+++ sumarr.js
@@ -1,1 +1,13 @@
-console.log("hello");
+// 1. Find the Sum of All Elements in an Array
+// Problem: Write a program that takes an array as input and calculates the sum of all its elements.
+// Example Input: [1, 2, 3, 4, 5]
+
+
+function sum(array){
+    let temp = 0;
+    for(i=0; i<array.length; i++){
+        temp += array[i];
+    }
+    console.log(temp);
+}
+sum([1, 2, 3, 4, 5]);
